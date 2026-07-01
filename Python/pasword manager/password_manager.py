19fe7c26a7e09781fd8eadb34655adef9de01954
Herpsime <negarse63@gmail.com>
2026-07-01 23:02:59 +0300

    added a function to search password

--- Python/pasword manager/password_manager.py
+++ Python/pasword manager/password_manager.py
@@ -27,3 +27,17 @@ def add(storage):
     storage["password"].append(new)
     
     return storage
+
+def search(storage):
+    search_web=input("input the web u want to search")
+    if search_web in storage["password"]:
+        search_user =input("enter your username")
+        for i in storage["password"]:
+            if i["username"]==search_user:
+                print("your password",i["password"])
+            else:
+                print("wrong username")
+    else:
+        print("there is no web like that")
+        
+    return storage        
